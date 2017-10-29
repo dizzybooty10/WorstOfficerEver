9c50d3698432268cada80d53ec8a3d58ec1b17d5
dizzybooty10 <boaralex2000@gmail.com>
2017-10-30 00:43:17 +0100

    qwe

--- app.js
+++ app.js
@@ -2,7 +2,19 @@
 var Discord = require('discord.js');
 var bot = new Discord.Client();
 
+var prefix = "!!";//the text before commands. (no commands added at the moment, except !!ping).
 
+
+function hasPrefix(var msg){
+
+if(msg.substring(0,prefix.length) == prefix){
+  return true;
+}
+else{
+  return false;
+}
+
+}
 
 
 //Listener Event: message recieved (this will run every time a message is recieved).
@@ -11,9 +23,12 @@ bot.on('message', message => {
   //variables
   var sender = message.author; //the person who sent the message
   var msg = message.content.toUpperCase(); //takes the message, and makes it all uppercase for easier management.
-  var prefix = '!!'; //the text before commands. (no commands added at the moment, except !!ping).
 
 
+if(hasPrefix(msg)){
+  message.channel.send('!!'); //sends PONG to the channel.
+
+}
 
   //ping / pong command for testing response time
   if (msg === prefix + 'PING') { //checks if the message sent by the sender is !!ping.
